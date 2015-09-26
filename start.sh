97285a124a914110ec69ea248a7dbf7f99c6b273
Damir <iscode@qq.com>
2015-09-26 22:13:21 +0800

    n

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-ln -s /home/damir/data/run/supervisord.conf  /usr/local/etc/supervisord.conf
-ln -sf /home/damir/data/run/site-enable /etc/nginx/sites-available/site-enable
-ln -sf /home/damir/data/run/site-enable /etc/nginx/sites-enabled/site-enable
+ln -sf /home/damir/data/site-enable /etc/nginx/sites-available/site-enable
+ln -sf /home/damir/data/site-enable /etc/nginx/sites-enabled/site-enable
 /usr/local/bin/supervisord
